69a5fce73b6679491f868da3b2159504df1d457c
pzsnark <pzsnark@gmail.com>
2020-11-17 20:59:54 +0400

    lesson_2

--- django/project_first/core/urls.py
+++ django/project_first/core/urls.py
@@ -1,7 +1,15 @@
 from django.conf.urls import url
-from .views import index
+from django.urls import path
+from .views import index, post_detail, post_create, post_edit, like_post, post_delete, feed
 
+app_name = 'core'
 
 urlpatterns = [
-    url('', index, name='index')  # name не обязательно, но желательно
+    url('^posts/$', index, name='index'),  # name не обязательно, но желательно
+    path('posts/<int:post_id/feed/', feed, name='post-feed'),
+    path('posts/<int:post_id/create/', post_create, name='post-create'),
+    url(r'^posts/(?P<post_id>[0-9]+)/$', post_detail, name='post-detail'),
+    path('posts/<int:post_id/edit/', post_edit, name='post-edit'),
+    path('posts/<int:post_id/delete/', post_delete, name='post-delete'),
+    path('posts/<int:post_id/like/', like_post, name='post-like'),
 ]
--- django/project_first/core/views.py
+++ django/project_first/core/views.py
@@ -1,7 +1,56 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from .models import Post
+from django.db.models import Sum
+from django.template import loader
+
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('Hello world')
+    popular_posts = Post.objects.annotate(likes_count=Sum('likes')).order_by('-likes_count')
+    template = loader.get_template('core/index.html')
+    context = {
+        'popular_posts': popular_posts,
+    }
+    return HttpResponse(template.render(context))
+
+
+def feed(request):
+    friends = request.user.profile.friends.all()
+    feed_posts = Post.objects.filter(author__in=friends)
+    return HttpResponse(feed_posts)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    # try:
+    #     post = Post.objects.get(id=post_id)
+    # except Post.DoesNotExist as e:
+    #     raise Http404('Post with id#{} does not exist'.format(post_id))
+    # post = Post.objects.get(id=post_id)
+    response = 'Детальное представление поста №{}, описание {}'.format(post.id, post.description)
+    return HttpResponse(response)
+
+
+def post_edit(request, post_id):
+    response = 'Редактирвоание поста №{}'.format(post_id)
+    return HttpResponse(response)
+
+
+def post_create(request):
+    response = 'Создание нового поста'
+    return HttpResponse(response)
+
+
+def post_delete(request, post_id):
+    response = 'Удаление поста №{}'.format(post_id)
+    return HttpResponse(response)
+
+
+def like_post(request, post_id):
+    response = 'Лайкнуть пост №{}'.format(post_id)
+    return HttpResponse(response)
+
+
+
