fd601e84fe99d7d392fd220b598890889bab16ec
PzSnark <pzsnark@gmail.com>
2021-02-25 10:00:32 +0400

    current

--- hw07_easy.py
+++ hw07_easy.py
@@ -52,15 +52,67 @@ class Triangle:
         print('Высота с точки C: ', self.hc)
 
 
-# print('Введите координаты точки A')
-# ax = float(input('x: '))
-# ay = float(input('y: '))
-# print('Введите координаты точки B')
-# bx = float(input('x: '))
-# by = float(input('y: '))
-# print('Введите координаты точки C')
-# cx = float(input('x: '))
-# cy = float(input('y: '))
+class Trap:
+    def __init__(self, ax, ay, bx, by, cx, cy, dx, dy):
+        self.ax = ax
+        self.ay = ay
+
+        self.bx = bx
+        self.by = by
+
+        self.cx = cx
+        self.cy = cy
+
+        self.cx = dx
+        self.cy = dy
+
+        self.ab = len_size(bx, ax, by, ay)
+        self.bc = len_size(cx, bx, cy, by)
+        self.cd = len_size(dx, cx, dy, cy)
+        self.da = len_size(dx, ax, dy, ay)
+        self.ac = len_size(cx, ax, cy, ay)
+        self.bd = len_size(dx, bx, dy, by)
+        # print(self.ab, self.bc, self.cd, self.da, self.ac, self.bd)
+
+        if self.check():
+            print('Фигура является трапецией')
+        else:
+            print('Фигура не является трапецией')
+
+        # фигура является равнобедренной трапецией если диагонали равны
+    def check(self):
+        if self.ac == self.bd:
+            return True
+        else:
+            return False
+
+    def perimeter(self):
+        if Trap.check(self):
+            print('Периметр трапеции: ', self.ab + self.bc + self.cd + self.da)
+        else:
+            print('Это не трапеция')
+
+    def area(self):
+        if Trap.check(self):
+            print('Площадь трапеции: ', (self.ab + self.cd / 4) * sqrt(4 * (self.bc ** 2) - ((self.ab - self.cd) ** 2)))
+        else:
+            print('Это не трапеция')
+
+
+ax = 1
+ay = 1
+bx = 2
+by = 6
+cx = 7
+cy = 6
+dx = 8
+dy = 1
+
+n = Trap(ax, ay, bx, by, cx, cy, dx, dy)
+
+n.check()
+n.perimeter()
+n.area()
 
 ax = -4
 ay = 7
