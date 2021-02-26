65f13e20b4dd2fb5a548c5e5bb6c2e5211130d14
PzSnark <pzsnark@gmail.com>
2021-02-26 16:29:04 +0400

    current

--- test.py
+++ test.py
@@ -1,72 +1,38 @@
-# Задача-2: Написать Класс "Равнобочная трапеция", заданной координатами 4-х точек.
-# Предусмотреть в классе методы:
-# проверка, является ли фигура равнобочной трапецией;
-# вычисления: длины сторон, периметр, площадь.
-
 from math import sqrt
+from abc import ABC, abstractmethod
+# a = {'x': 4, 'y': 7}
+# b = {'x': 7, 'y': 4}
 
 
-def len_size(a, b, c, d):
-    return sqrt(((b - a) ** 2) + ((d - c) ** 2))
+class Figure(ABC):
+    """Абстрактный класс для геометрических фигур"""
 
-
-class Trap:
-    def __init__(self, ax, ay, bx, by, cx, cy, dx, dy):
-        self.ax = ax
-        self.ay = ay
-
-        self.bx = bx
-        self.by = by
-
-        self.cx = cx
-        self.cy = cy
-
-        self.cx = dx
-        self.cy = dy
-
-        self.ab = len_size(bx, ax, by, ay)
-        self.bc = len_size(cx, bx, cy, by)
-        self.cd = len_size(dx, cx, dy, cy)
-        self.da = len_size(dx, ax, dy, ay)
-        self.ac = len_size(cx, ax, cy, ay)
-        self.bd = len_size(dx, bx, dy, by)
-
-        if self.is_isosceles:
-            print('Фигура является трапецией')
-        else:
-            print('Фигура не является трапецией')
+    @staticmethod
+    def side_lengths(a, b):
+        return sqrt(((b['x'] - a['x']) ** 2) + ((b['y'] - a['y']) ** 2))
 
     @property
-    def is_isosceles(self):
-        if self.ac == self.bd:
-            return True
-        else:
-            return False
-
     def perimeter(self):
-        if self.is_isosceles:
-            print('Периметр трапеции: ', self.ab + self.bc + self.cd + self.da)
-        else:
-            print('Это не трапеция')
+        return sum(self.sides)
 
+    @property
     def area(self):
-        if self.is_isosceles:
-            print('Площадь трапеции: ', (self.ab + self.cd / 4) * sqrt(4 * (self.bc ** 2) - ((self.ab - self.cd) ** 2)))
-        else:
-            print('Это не трапеция')
+        return
 
 
-ax = 1
-ay = 1
-bx = 2
-by = 6
-cx = 7
-cy = 6
-dx = 8
-dy = 1
+class Triangle(Figure):
 
-n = Trap(ax, ay, bx, by, cx, cy, dx, dy)
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
 
-n.check()
-n.perimeter()
-n.area()
+        self.side_a = self.side_lengths(b, c)
+        self.side_b = self.side_lengths(a, c)
+        self.side_c = self.side_lengths(a, b)
+        self.sides = [self.side_a, self.side_b, self.side_c]
+
+
+tri = Triangle({'x': 4, 'y': 7}, {'x': 1, 'y': 2}, {'x': 7, 'y': 4})
+print(tri.side_a, tri.side_b, tri.side_c)
+print(tri.perimeter)
