69bc13be2b400f391836dc7ee6c1504415731e4c
PzSnark <pzsnark@gmail.com>
2021-03-01 22:35:05 +0400

    current

--- hw07_hard.py
+++ hw07_hard.py
@@ -10,3 +10,42 @@
 # С использованием классов.
 # Реализуйте классы сотрудников так, чтобы на вход функции-конструктора
 # каждый работник получал строку из файла
+
+
+def create_worker(path):
+    with open(path, mode='r', encoding='utf-8') as f:
+        worker_list = []
+        for line in f:
+            worker_list.append(Worker(*line.split()))
+        return worker_list
+
+
+class Worker:
+
+    def __init__(self, name, surname, salary, position, clock_rate):
+        self.__name = name
+        self.__surname = surname
+        self.__salary = salary
+        self.__position = position
+        self.__clock_rate = clock_rate
+        self.__path = './hw7_hard/data/workers'
+
+    def info(self):
+        return self.__name, self.__surname, self.__salary, self.__position, self.__clock_rate
+
+    def add_worker(self, value):
+        with open(self.__path, mode='r', encoding='utf-8') as f:
+            worker_list = []
+            for line in f:
+                worker_list.append(Worker(*line.split()))
+            return worker_list[value]
+
+
+# for worker in create_worker('./hw7_hard/data/workers')[1:]:
+#     print(worker.info())
+#
+# worker1 = create_worker('./hw7_hard/data/workers')[1]
+# print(worker)
+
+worker1 = Worker.add_worker('self', 2)
+print(worker1)
--- test.py
+++ test.py
@@ -1,38 +1,5 @@
-from math import sqrt
-from abc import ABC, abstractmethod
-# a = {'x': 4, 'y': 7}
-# b = {'x': 7, 'y': 4}
+class Worker:
 
-
-class Figure(ABC):
-    """Абстрактный класс для геометрических фигур"""
-
-    @staticmethod
-    def side_lengths(a, b):
-        return sqrt(((b['x'] - a['x']) ** 2) + ((b['y'] - a['y']) ** 2))
-
-    @property
-    def perimeter(self):
-        return sum(self.sides)
-
-    @property
-    def area(self):
-        return
-
-
-class Triangle(Figure):
-
-    def __init__(self, a, b, c):
-        self.a = a
-        self.b = b
-        self.c = c
-
-        self.side_a = self.side_lengths(b, c)
-        self.side_b = self.side_lengths(a, c)
-        self.side_c = self.side_lengths(a, b)
-        self.sides = [self.side_a, self.side_b, self.side_c]
-
-
-tri = Triangle({'x': 4, 'y': 7}, {'x': 1, 'y': 2}, {'x': 7, 'y': 4})
-print(tri.side_a, tri.side_b, tri.side_c)
-print(tri.perimeter)
+    with open('./hw7_hard/data/workers', mode='r', encoding='utf-8') as f:
+        for line in f:
+            print(line)
