9323b38b7dbcaafc8a5588cbf8f2c747d155ff99
PzSnark <pzsnark@gmail.com>
2021-03-02 22:05:43 +0400

    current

--- hw07_hard.py
+++ hw07_hard.py
@@ -25,27 +25,45 @@ class Worker:
     def __init__(self, name, surname, salary, position, clock_rate):
         self.__name = name
         self.__surname = surname
-        self.__salary = salary
+        self.__salary = int(salary)
         self.__position = position
-        self.__clock_rate = clock_rate
+        self.__clock_rate = int(clock_rate)
         self.__path = './hw7_hard/data/workers'
+        self.__clock_fact = None
+        self.__pay = None
 
     def info(self):
         return self.__name, self.__surname, self.__salary, self.__position, self.__clock_rate
 
-    def add_worker(self, value):
-        with open(self.__path, mode='r', encoding='utf-8') as f:
-            worker_list = []
-            for line in f:
-                worker_list.append(Worker(*line.split()))
-            return worker_list[value]
+    @property
+    def clock_fact(self):
+        return self.__clock_fact
+
+    def set_clock_fact(self):
+        if self.__clock_fact is None:
+            print('Значение не установлено')
+            self.__clock_fact = input('Введите новое значение: ')
+        else:
+            print(f'Текущее значение: {self.__clock_fact}')
+            self.__clock_fact = input('Введите новое значение: ')
+
+    def payroll(self):
+        pay_per_clock = self.__salary / self.__clock_rate
+        if self.__clock_fact == self.__clock_rate:
+            self.__pay = self.__salary
+        elif self.__clock_fact < self.__clock_rate:
+            self.__pay = self.__salary - (self.__clock_fact * pay_per_clock)
+        else:
+            self.__pay = self.__salary + (self.__clock_fact * (pay_per_clock * 2))
 
 
-# for worker in create_worker('./hw7_hard/data/workers')[1:]:
+# for worker in create_workers_from_file('./hw7_hard/data/workers')[1:]:
 #     print(worker.info())
 #
-# worker1 = create_worker('./hw7_hard/data/workers')[1]
-# print(worker)
+# worker1 = create_workers_from_file('./hw7_hard/data/workers')[1]
+# print(worker1)
 
-create_workers_from_file('./hw7_hard/data/workers')
 
+workers = create_workers_from_file('./hw7_hard/data/workers')
+workers[1].set_clock_fact()
+print(workers[1].clock_fact)
