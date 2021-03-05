11c2f9985ebe9a98b0f16246a639bf506a8bb0c1
PzSnark <pzsnark@gmail.com>
2021-03-06 00:10:44 +0400

    current

--- loto.py
+++ loto.py
@@ -7,11 +7,58 @@ class Card:
     """Класс описывающий карты лото"""
 
     def __init__(self):
-        self.__card = [['  '] * 9] * 3
-        self.__point = [self.__card[0][2]]
-
-    def generator(self):
         pass
-        return self.__card
+
+    @staticmethod
+    def generator():
+        card = [[0] * 9] * 3
+        rnd = list(range(1, 30))
+        random.shuffle(rnd)
+
+        for x in range(3):
+            for y in range(9):
+                card[x][y] = (rnd[(x * 10) + y])
+            card[x] = sorted(card[x])
+            true = 0
+            false = 0
+            for y in range(9):
+                if random.choice([True, False]) is True and true <= 4 or false >= 4:
+                    true += 1
+                else:
+                    (card[x][y]) = '  '
+                    false += 1
+        return card
+
+    @staticmethod
+    def game():
+        Card.move()
+
+    @staticmethod
+    def move():
+        keg = list(range(1, 90))
+        random.shuffle(keg)
+        count = 0
+
+        print(f'Новый бочонок: {keg[count]} (Осталось {89 - count})')
+        print('----- Ваша карточка ------')
+        for x in range(3):
+            for y in range(9):
+                if len(str(card_gamer[x][y])) < 2:
+                    card_gamer[x][y] = ' ' + str(card_gamer[x][y])
+                print(f'{str(card_gamer[x][y])}', end=' ')
+            print('\r')
+        print('--------------------------')
+
+        print('-- Карточка компьютера ---')
+        for x in range(3):
+            for y in range(9):
+                if len(str(card_computer[x][y])) < 2:
+                    card_computer[x][y] = ' ' + str(card_computer[x][y])
+                print(f'{str(card_computer[x][y])}', end=' ')
+            print('\r')
+        print('--------------------------')
 
 
+card_gamer = (Card.generator())
+card_computer = (Card.generator())
+Card.move()
--- test.py
+++ test.py
@@ -1,22 +1,33 @@
 import random
 
-i = 1
-lst = []
-while i < 45:
-    lst.append(random.randint(1, 90))
-    i += 1
-
-lst2 = random.sample(lst, 15)
-
+# i = 1
+# lst = []
+# while i < 45:
+#     lst.append(random.randint(1, 90))
+#     i += 1
+#
+# lst2 = random.sample(lst, 15)
+#
 # print(lst)
 # print(lst2)
 
+# генерация последовательности без повторов
 
-card = [['  '] * 9] * 3
-for n in card[0]:
-    pass
+
+card = [[], [], []]
+rnd = list(range(1, 30))
+random.shuffle(rnd)
+
+for x in range(0, 3):
+    for y in range(0, 9):
+        card[x].append(rnd[(x * 10)+y])
+    card[x] = sorted(card[x])
+
+
 
 print('--------------------------')
 for element in card:
     print(element)
 print('--------------------------')
+
+# print(random.sample(card[0], 5))
